974743f581b1b785b352a2c2d5cc64cd6a35da10
unknown <nami0879@naver.com>
2024-01-24 22:53:17 +0900

    conf

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/deploy
-cd $REPOSITORY 
-
-# JAR_NAME=$(ls $REPOSITORY | grep '.jar' | tail -n 1) 
-# JAR_PATH=$REPOSITORY/$JAR_NAME
-
-# APP_NAME=diary-front
-# CURRENT_PID=$(pgrep -fl diary-api | grep java | awk '{print $1}')
-
-# if [ -z "$CURRENT_PID" ]; then
-#     echo "NOT RUNNING"
-# else
-#     echo "> kill -9 $CURRENT_PID"
-#     kill -15 $CURRENT_PID
-#     sleep 5
-# fi
-
-# echo "> $JAR_PATH 배포"
-# nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+REPOSITORY=/home/deploy
+cd $REPOSITORY
