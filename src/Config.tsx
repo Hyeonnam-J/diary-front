cb9eaf875113d2162e43b81b02763b4914d2db4a
unknown <nami0879@naver.com>
2024-01-28 17:38:53 +0900

    server

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,5 +1,6 @@
-export const SERVER_IP = 'https://diary-api.com';
 // export const SERVER_IP = 'http://localhost:8080';
+// export const SERVER_IP = 'https://diary-api.shop';
+export const SERVER_IP = 'https://api.my-diary.life';
 
 export const Page = {
   perPageSize: 10,
