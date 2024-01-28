dacd5a50a899aa191903fed678504c9cfb485114
unknown <nami0879@naver.com>
2024-01-28 19:25:47 +0900

    adapt

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,6 +1,6 @@
 // export const SERVER_IP = 'http://localhost:8080';
 // export const SERVER_IP = 'https://diary-api.shop';
-export const SERVER_IP = '43.203.97.230';
+export const SERVER_IP = 'https://43.203.97.230';
 
 export const Page = {
   perPageSize: 10,
