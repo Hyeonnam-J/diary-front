8d5113e7c4acab40135eb198df92d886a29d7348
unknown <nami0879@naver.com>
2024-01-28 19:31:31 +0900

    adapt

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,7 +1,7 @@
 // export const SERVER_IP = 'http://localhost:8080';
-// export const SERVER_IP = 'https://diary-api.shop';
+export const SERVER_IP = 'https://diary-api.shop';
 // export const SERVER_IP = 'https://43.203.97.230';
-export const SERVER_IP = 'https://api.my-diary.life';
+// export const SERVER_IP = 'https://api.my-diary.life';
 
 export const Page = {
   perPageSize: 10,
